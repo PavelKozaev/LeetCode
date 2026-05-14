8792bb3c514a2427b34b702618758006d36ff1f0
Pavel Kozaev <pkozaev221299@gmail.com>
2026-05-14 11:29:10 +0300

    feat: add solution for 125. Valid Palindrome

--- src/LeetCode/Program.cs
+++ src/LeetCode/Program.cs
@@ -1,13 +1,20 @@
 ﻿using LeetCode.TwoPointers;
 
-var s = new[] {'h', 'e', 'l', 'l', 'o'};
-var t = new[] {'H', 'a', 'n', 'n', 'a', 'h'};
+// var s = new[] {'h', 'e', 'l', 'l', 'o'};
+// var t = new[] {'H', 'a', 'n', 'n', 'a', 'h'};
+//
+// Console.WriteLine(s);
+// Console.WriteLine(t);
+//
+// TwoPointers.ReverseString(s);
+// TwoPointers.ReverseString(t);
+//
+// Console.WriteLine(s);
+// Console.WriteLine(t);
 
-Console.WriteLine(s);
-Console.WriteLine(t);
 
-TwoPointers.ReverseString(s);
-TwoPointers.ReverseString(t);
 
-Console.WriteLine(s);
-Console.WriteLine(t);
+
+var s = "A man, a plan, a canal: Panama";
+
+Console.WriteLine(TwoPointers.IsPalindrome(s));
