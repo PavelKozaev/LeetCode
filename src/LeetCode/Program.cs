862444c248b95c9488a8456e86cb2bdae6fd052a
PavelKozaev <pkozaev221299@gmail.com>
2026-05-16 14:00:31 +0300

    eat: add solution for 349. Intersection of Two Arrays

--- src/LeetCode/Program.cs
+++ src/LeetCode/Program.cs
@@ -22,9 +22,17 @@
 
 
 
-var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
-var m = 3;
-var nums2 = new[] { 2, 5, 6 };
-var n = 3;
+// var nums1 = new[] { 1, 2, 3, 0, 0, 0 };
+// var m = 3;
+// var nums2 = new[] { 2, 5, 6 };
+// var n = 3;
+//
+// TwoPointers.Merge(nums1, m, nums2, n);
 
-TwoPointers.Merge(nums1, m, nums2, n);
+
+
+
+var nums1 = new[] { 1, 2, 2, 1 };
+var nums2 = new[] { 2, 2 };
+
+TwoPointers.Intersection(nums1, nums2);
--- src/LeetCode/TwoPointers/TwoPointers.cs
+++ src/LeetCode/TwoPointers/TwoPointers.cs
@@ -74,4 +74,39 @@ public static class TwoPointers
             k--;
         }
     }
+
+    public static int[] Intersection(int[] nums1, int[] nums2)
+    {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+        var result = new List<int>();
+
+        var i = 0;
+        var j = 0;
+
+        while (i < nums1.Length && j < nums2.Length)
+        {
+            if (nums1[i] == nums2[j])
+            {
+                if (result is { Count: 0 } || result[^1] != nums1[i])
+                {
+                    result.Add(nums1[i]);
+                }
+                
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        
+        return result.ToArray();
+    }
 }
